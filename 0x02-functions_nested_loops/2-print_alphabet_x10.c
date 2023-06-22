b0dc4ed9eab526bd5df82deec8a32834c39d71b2
Ernest-bless <blessernesto21@gmail.com>
2023-06-21 21:35:47 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,17 +6,14 @@
  */
 void print_alphabet_x10(void)
 {
-int x;
-int y;
-
-for (x = 1; x <= 10; x++)
-{
-for (y = 97; y <= 122; y ++)
-{
+     int x;
+     int y;
+     for (x = 1; x <= 10; x++)
+         {
+            for (y = 97; y <= 122; y ++)
+                   {
 	_putchar(y);
-}
+                   }
         _putchar('\n');
-}
-
-
+         }
 }
