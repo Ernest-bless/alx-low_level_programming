34bd8f76f8e6dcc5270e5b83e4e5d146b560890f
Ernest-bless <blessernesto21@gmail.com>
2023-07-27 20:39:38 -0700

    0x12. C - Singly linked lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,42 +1,54 @@
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
 #include "lists.h"
+
 /**
- * add_node_end - a function that adds a new node
- * at the end of a list_t list.
- * @head: input
- * @str: input
- * Return: 0
+ * add_node_end - Adds a new node at the end of a list_t list
+ *
+ * @head: Double pointer to the head of the list
+ * @str: Pointer to the string to be added
+ *
+ * Return: The address of the new element, or NULL if it failed
  */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
-	int i, n = 0;
-	list_t *new;
-	list_t *temp;
+	list_t *new_node, *current;
+	char *new_str;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-
-	for (i = 0; str[i] != '\0'; i++)
-		n++;
-
-	new->len = i;
-	new->str = strdup(str);
-	new->next = NULL;
-
-	if (*head == NULL)
+	/* Allocate memory for the new node */
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
 	{
-		*head = new;
-		return (new);
+		return (NULL);
 	}
 
-	temp = *head;
+	/* Duplicate the string */
+	new_str = strdup(str);
+	if (new_str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-	while (temp->next != NULL)
-		temp = temp->next;
+	/* Initialize the new node */
+	new_node->str = new_str;
+	new_node->len = strlen(str);
+	new_node->next = NULL;
 
-	temp->next = new;
-
-	return (new);
+	/* Add the new node at the end of the list */
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		current = *head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new_node;
+	}
+	return (new_node);
 }
